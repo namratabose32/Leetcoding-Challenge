680316ff64f353f8929f75454014e429dd58de36
Namrata <74545248+namratabose32@users.noreply.github.com>
2021-09-09 02:51:53 +0530

    Update Day8.cpp

--- September/Day8.cpp
+++ September/Day8.cpp
@@ -19,3 +19,13 @@ public:
         }return ans;
     }
 };
+
+//modified
+
+string shiftingLetters2(string S, vector<int> shifts) {
+        for (int i = shifts.size() - 2; i >= 0; i--)
+            shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+        for (int i = 0; i < shifts.size(); i++)
+            S[i] = (S[i] - 'a' + shifts[i]) % 26 + 'a';
+        return S;
+    }
